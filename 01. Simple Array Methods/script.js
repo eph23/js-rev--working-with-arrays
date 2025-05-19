b2e97892cdf8519d5707334ca5b7e0779c728624
ephraim <ephraims.818@gmail.com>
2025-05-19 20:19:13 +0600

    JS-REV--WORKING-WITH-ARRAYS - 01. Simple Array Methods

---
+++ 01. Simple Array Methods/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+// The slice method
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+// Splice
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// Reverse
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// Concat
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// Join method
